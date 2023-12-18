82b68a94f10f0cf05493414075d2b8ea708f4235
Idoko Agada Jerry <Idokoidoko4@gmail.com>
2023-12-18 12:57:55 +0100

    gtb chatbot

--- portfolio/src/ExperienceCard.tsx
+++ portfolio/src/ExperienceCard.tsx
@@ -7,21 +7,14 @@ import st_mary from "./assets/images/st_mary.jpeg";
 import gtbsvg from "./assets/images/gtblogo.svg";
 
 const cardDetails = [
+
   {
-    icon: gtbsvg,
+     icon: gtbsvg,
     title: "Full stack developer",
-    description: "I work with a team that developed and hosted the Steve OCPP EV server for electrical vehicle charging stations.",
-    company: "GTB",
-    address:"Lekki, Lagos Nigeria",
-    date: "2023 - Present",
-  },
-  {
-    icon: cbc,
-    title: "Full stack developer",
-    description: "I am part of the team that developed chatbot web services using whatsapp, CRM as well as NLP. In this website below(bottom right) is the web widget for the chatbot",
+    description: "I am part of the team that developed chatbot using whatsapp, CRM as well as NLP. This is a GTB webservices that enable customer solve their problems. In this website below( bottom right) is the web widget for the chatbot",
     company: "CBCEmea Gedu Tech",
     address:"Lekki, Lagos Nigeria",
-    date: "June 2022 - Present",
+    date: "June 2023",
   },
   {
     icon: cbc,
